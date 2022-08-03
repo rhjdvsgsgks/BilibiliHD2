e2dd99fea9283994a85b36c1100dc4ec8ea1f657
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2022-08-03 13:56:48 +0000

    f

--- app/src/main/java/com/duzhaokun123/bilibilihd2/ui/widget/PlayerView.kt
+++ app/src/main/java/com/duzhaokun123/bilibilihd2/ui/widget/PlayerView.kt
@@ -58,7 +58,7 @@ class PlayerView @JvmOverloads constructor(
                     trackSelectorBinding.root.removeFromParent()
                 }
                 val trackSelector = (player as ExoPlayer).trackSelector as DefaultTrackSelector
-                (player as ExoPlayer).setPlaybackParameters(PlaybackParameters(2.0))
+                (player as ExoPlayer).setPlaybackParameters(PlaybackParameters(2.0f))
                 val renderers = (0 until  (trackSelector.currentMappedTrackInfo?.rendererCount ?: 0)).map { i ->
                     trackSelector.currentMappedTrackInfo?.getRendererName(i)
                 }.toTypedArray()
